b958336aedccea298c98d0e05c37283a038ccb75
ShaikhArmaan27 <72267363+ShaikhArmaan27@users.noreply.github.com>
2020-11-24 21:47:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+
+
+function setup() {
+	createCanvas(1600, 700);
+
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	roof1=new roof(800,175,225,20);
+
+	bob1=new bob(720,675,20);
+	bob2=new bob(760,675,20);
+	bob3=new bob(800,675,20);
+	bob4=new bob(840,675,20);
+	bob5=new bob(880,675,20);
+
+
+	rope1=new rope(bob1.body,roof1.body,-80, 0)
+	rope2=new rope(bob2.body,roof1.body,-40, 0)
+	rope3=new rope(bob3.body,roof1.body,0, 0)
+	rope4=new rope(bob4.body,roof1.body,40, 0)
+	rope5=new rope(bob5.body,roof1.body,80, 0)
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+
+  background(230);
+
+  roof1.display();
+
+  rope1.display()
+  rope2.display()
+  rope3.display()
+  rope4.display()
+  rope5.display()	
+
+
+
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
+}
+
+
+
+function mouseDragged(){
+    Matter.Body.setPosition(bob1.body, {x: mouseX , y: mouseY});
+
+}
+
+
+
+
